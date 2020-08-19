4a8ffd1767b39ce9a5f8a2bb61089aa6a439a3f2
cfredberg <53453343+cfredberg@users.noreply.github.com>
2020-08-19 16:56:42 -0700

    Update _00_ArrayCheatSheet.java

--- src/arrays/_00_ArrayCheatSheet.java
+++ src/arrays/_00_ArrayCheatSheet.java
@@ -1,27 +1,46 @@
 package arrays;
 
+import java.util.Random;
+
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
 		//1. make an array of 5 Strings
-		
+		String[] arr = {"a", "b", "c", "d", "e"};
 		//2. print the third element in the array
-		
+		System.out.println(arr[2]);
 		//3. set the third element to a different value
-		
+		arr[2] = "g";
 		//4. print the third element again
-		
+		System.out.println(arr[2]);
 		//5. use a for loop to print all the values in the array
 		//   BE SURE TO USE THE ARRAY'S length VARIABLE
-		
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
 		
 		//6. make an array of 50 integers
-		
+		int[] array = new int[50];
+		int check = 100;
 		//7. use a for loop to make every value of the integer array a random number
-		
+		for (int i = 0; i < array.length; i++) {
+			array[i] = new Random().nextInt(100);
+			if (array[i] < check) {
+				check = array[i];
+			}
+		}
 		//8. without printing the entire array, print only the smallest number in the array
-		
+		System.out.println(check);
 		//9 print the entire array to see if step 8 was correct
-		
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
+		int check1 = 0;
 		//10. print the largest number in the array.
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] > check1) {
+				check1 = array[i];
+			}
+		}
+		System.out.println(check1);
 	}
 }
